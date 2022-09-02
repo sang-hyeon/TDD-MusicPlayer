49a12a83b1d46ec83941cb7997a44345442604a7
상현 김 <thwj@me.com>
2022-09-02 22:39:48 +0900

    테스트 도구인 Generate를 작성합니다.

---
+++ src/TDD-MusicPlayer.UnitTest/Tools/Generate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace TDD_MusicPlayer.UnitTest.Tools
+{
+    public static class Generate
+    {
+        public static IEnumerable<Song> Songs(int length = 3)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                yield return new Song("a" + i, "b" + i);
+            }
+        }
+
+        public static Song[] SongsAsArray(int length = 3)
+        {
+            return Songs(length).ToArray();
+        }
+
+        public static List<Song> SongsAsList(int length  = 3)
+        {
+            return Songs(length).ToList();
+        }
+
+        public static ImmutableArray<Song> SongsAsImmutable(int length = 3)
+        {
+            return Songs(length).ToImmutableArray();
+        }
+    }
+}
