c430943307be9a5733fee1f1532d401d6338bbb9
상현 김 <thwj@me.com>
2022-09-02 22:54:54 +0900

    Track을 작성합니다.

---
+++ src/TDD-MusicPlayer/Track.cs
@@ -0,0 +1,13 @@
+﻿namespace TDD_MusicPlayer
+{
+    public record Track : Song
+    {
+        public int TrackNumber { get; }
+
+        public Track(int trackNumber, Song song)
+            : base(song.Artist, song.Title)
+        {
+            this.TrackNumber = trackNumber;
+        }
+    }
+}
